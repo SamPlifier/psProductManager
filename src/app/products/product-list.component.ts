52effb92e8cce3e0a3da730684a956d10e8d4c19
SamDC <sam.davis.castro@gmail.com>
2019-01-10 13:45:06 -0500

    toggle product image button added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   //typescript declaration when data type is unknown or unimportant
   products: any[] = [{
     "productId": 2,
@@ -27,4 +30,8 @@ export class ProductListComponent {
       "starRating": 4.8,
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
   }];
+  // specified return type as void since it doesn't have one
+  toggleImage(): void {
+      this.showImage = !this.showImage;
+  }
 }
