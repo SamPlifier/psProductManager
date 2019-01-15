0ca7706df332fa035a18c98f216f0a2c8c18d15f
SamDC <sam.davis.castro@gmail.com>
2019-01-14 23:00:59 -0500

    split into feature module and root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convertToSpaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
+    ProductModule,
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-        { path: 'products', component: ProductListComponent },
-        { path: 'products/:id',
-            canActivate: [ ProductDetailGuard ],
-            component: ProductDetailComponent },
         { path: 'welcome', component: WelcomeComponent },
         { path: '', redirectTo: 'welcome', pathMatch: 'full'},
         { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
